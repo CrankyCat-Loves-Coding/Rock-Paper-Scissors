465fbefd7429d30134d4daf72b1ca15e55a0da89
CrankyCat-Loves-Coding <crankycatlovecoding@outlook.com>
2021-11-11 22:53:30 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Greeting
+name = input("What is your name?").title()
+print("Hello " + name + "! Would you like to play Rock Paper Scissors?")
+
+answer = input("Yes/Y or No/N").lower()
