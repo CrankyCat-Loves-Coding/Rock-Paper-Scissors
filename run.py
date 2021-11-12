0f499a27f22fdaa53270917593945fd6d07a78ca
CrankyCat-Loves-Coding <crankycatlovecoding@outlook.com>
2021-11-12 22:19:40 +0000

    Add code to allow player to start the game or exit the game

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
-#Greeting
-name = input("What is your name?").title()
-print("Hello " + name + "! Would you like to play Rock Paper Scissors?")
+import random
 
-answer = input("Yes/Y or No/N").lower()
+#Start Game
+
+play_game = True
+
+while play_game:
+
+    name = input("What is your name?").title()
+    print("Hello " + name + "! Would you like to play Rock Paper Scissors? y/n ")
+
+    answer = input().lower()
+
+    if answer.lower() == "y":
+        print("Learn how to play")
+    elif answer.lower() == "n":
+        print("Exit game")
+        play_game = False
+    else:
+        print("Please enter y or n :)")
+print("Thank you for playing. Hope to see you again ^8^ ")
