6a45d760c75df1f1af1797cba535e1568ea0d821
CrankyCat-Loves-Coding <crankycatlovecoding@outlook.com>
2021-11-13 01:26:58 +0000

    loop the game until the player wins 3 games

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def play():
     """
@@ -9,16 +10,41 @@ def play():
     computer = random.choice(['r','p','s'])
     
     if player == computer:
-        return "You and the computer have both chosen {}. It's a tie.".format(computer)
+        return (0,player,computer)
     if win(player, computer):
-        return "You have chosen {} \n the computer has chosen {}.\n You won! \n".format(player, computer)
-    return "You have chosen {} \n the computer has chosen {}.\n You lost! \n".format(player, computer)
+        return (1,player,computer)
+    return (-1,player,computer)
 
 
 def win(a, b):
+    """
+    the player won will return to be true
+    r > s, s > p, p > r
+    """
     if (a == 'r' and b == 's') or (a == 's' and b == 'p') or (a == 'p' and b == 'r'):
         return True
     return False
+
+
+def player_win(n):
+    """
+    play with the computer until player wins 3 games
+    """
+    player_wins = 0
+    wins_count = math.ceil(n/2)
+
+    while player_wins < wins_count and player_wins < 3:
+        result, player, computer = play()
+        if result == 0:
+            print("It's a tie. \n You have chosen {}. \n The computer has chosen {}. \n".format(player, computer))
+        elif result == 1:
+            player_wins += 1
+            print("You won! ^^. \n You have chosen {}. \n The computer has chosen {}. \n".format(player, computer))
+        else:
+            print("You lost! ^^. \n You have chosen {}. \n The computer has chosen {}. \n".format(player, computer))
+        print()
+    
+
 
 
 #Ask for player name and if they want to play the game or exit the game
@@ -35,7 +61,7 @@ while play_game:
     if answer.lower() == "y":
         print("\t Play Rock-Paper-Scissors with computer to display a picture after 3 wins! \n")
         print("\t 'r' for Rock, 'p' for Paper, 's' for Scissors, 'q' for Quit \n ")
-        print(play())
+        player_win(3)
 
     elif answer.lower() == "n":
         print("Exit game")
