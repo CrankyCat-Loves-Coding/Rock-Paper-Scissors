2a77bc1dca0629943462e4e9498a5f3eb462081b
CrankyCat-Loves-Coding <crankycatlovecoding@outlook.com>
2021-11-16 00:33:44 +0000

    add detail descriptions to each block of codes inside the start function

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import random
 import math
 import time
 
+
 def welcome():
     """
     add welcome page
@@ -29,8 +30,10 @@ def start():
     the program will first obtain player's name
     then greet to the player
     game starts when player enter y
-    game exit when play enter n
+    game exit when player enter n
     """
+    # obtain play's name and greet to the player
+    # ask to start or exit the game
     name = input("What is your name? \n").title()
     print("\nHello " + name + "!ヽ(^o^)丿\n")
     time.sleep(1)
@@ -38,21 +41,25 @@ def start():
     time.sleep(2)
     print("Please enter 'y' for Yes and 'n' for No:\n")
 
+    # start game
     play_game = True
 
     while play_game:
-
+        # check input y and n
+        # if input other than y or n
+        # the program will ask the player to enter either y or n
         answer = input().lower()
         answers = ['y', 'n']
 
         while answer not in answers:
-            answer = input("\nPlease enter 'y' or 'n' to start or exit the game :)\n").lower()
-
+            answer = input("\nEnter 'y' or 'n' to start or exit the game :)\n")
+            answer = input().lower()
+        # game start if y entered
         if answer.lower() == "y":
             print("\nPlay Rock-Paper-Scissors with computer to display a picture after 3 wins! ")
             print("\n'r' for Rock, 'p' for Paper, 's' for Scissors \n ")
             player_win(5)
-
+        # exit game if n entered
         elif answer.lower() == "n":
             print("Exit game")
             print("Thank you for playing. Hope to see you again ^8^ ")
